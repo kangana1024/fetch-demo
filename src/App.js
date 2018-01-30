a849ff08f1a64e08956a2d2dc91569ae83f7ca48
kangana512 <kangana512@gmail.com>
2018-01-30 13:56:49 +0700

    Demo fetch

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import fetch from 'isomorphic-fetch';
+import { Layout, Card, List, Icon, Spin } from 'antd';
+import './App.css';
+
+class App extends Component {
+  componentDidMount = async () => {
+    let data = await fetch('http://59b1622bffff010011b4efa0.mockapi.io/api/v1/todo').then((res) => res.json())
+    let dataSource = data.result.data
+    console.log(data)
+    this.setState({ dataSource: dataSource })
+  }
+  constructor(props) {
+    super(props);
+    this.state = {
+      dataSource: []
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+      {this.state.dataSource.length>0?
+      <Card style={{ width: 500 }}>
+        <List
+          bordered
+          dataSource={this.state.dataSource}
+          renderItem={(item, index) => (
+            <List.Item>
+              {item}
+            </List.Item>
+          )}
+        />
+        </Card>:<Spin />}
+      </div>
+    );
+  }
+}
+
+export default App;
